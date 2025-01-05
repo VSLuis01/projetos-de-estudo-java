b53cc132f7f49b1ec66e21154317f49fd65042fb
Luis Eduardo <vsluiseduardo01@gmail.com>
2025-01-05 13:56:42 -0400

    Criado controller do autores.

---
+++ libraryapi/src/main/java/br/com/luisedu/libraryapi/controller/AutorController.java
@@ -0,0 +1,12 @@
+package br.com.luisedu.libraryapi.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/autores")
+// baseUrl: http://localhost:8080/autores
+public class AutorController {
+
+}
